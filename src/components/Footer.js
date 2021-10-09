ec44a2009b2ce676f2cfed13be56838f42c50893
The Akshay <akshay@debian>
2021-10-09 17:48:30 +0530

    Initial Commit Navbar successfully created

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import './footer.css'
+
+
+
+
+const Footer=()=>{
+
+    return(
+        <div className="footer">
+            ©The Akshay PVT. LTD.
+            <br />
+            <a href="https://www.twitter.com/_the_Akshay">@_the_Akshay</a>
+
+        </div>
+    );
+}
+
+export default Footer
