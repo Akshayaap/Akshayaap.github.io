79d35853ad41f26051fbb9a376e7e914d606de0a
akshay <akshaykumarparmar51@gmail.com>
2022-01-07 17:19:48 +0530

    HomePage Looks Good but ..

--- src/components/homepage/right-pane/RPItem.js
+++ src/components/homepage/right-pane/RPItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import './rightpane.css';
 const RPItem=(arg1,arg2,arg3)=>{
     return (
         <div className="rp-item">
-
+            <h3>arg1</h3>
+            <h4>arg2</h4>
+            <h4>arg3</h4>
         </div>
     )
 }
--- src/components/homepage/right-pane/RightPane.js
+++ src/components/homepage/right-pane/RightPane.js
@@ -1,30 +1,44 @@
 import React from 'react';
-import {Container,Row,Col} from 'react-bootstrap';
-import RPItem  from './RPItem';
+import { Container, Row, Col } from 'react-bootstrap';
+import RPItem from './RPItem';
+import './rightpane.css';
 
-const RighPane=()=>{
+const RighPane = () => {
+
+    const [items, setItems] = React.useState([['item1', 'item2', 'item3'], 
+                                                ['item4', 'item5', 'item6'], 
+                                                ['item7', 'item8', 'item9'], 
+                                                ['item10', 'item11', 'item12'],
+                                                [ 'item13', 'item14', 'item15'],
+                                                [ 'item16', 'item17', 'item18'],
+                                                [ 'item19', 'item20', 'item21'],
+                                                ['item22', 'item23', 'item24'],
+                                                ['item25', 'item26', 'item27'],
+                                                ['item28', 'item29', 'item30'],
+                                                ['item31', 'item32', 'item33'],
+                                                ['item34', 'item35', 'item36'],
+                                                ['item37', 'item38', 'item39'],
+                                                ['item40', 'item41', 'item42'],]);
+
+
     return (
-        <Container>
-                    <Row className="rp-item">
-                        <Col>   
-                            <RPItem arg>
+        <Container className='right'>
+            {
+                items.map((item, index) => {
+                    return (
 
-                            </RPItem>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            
-                        </Col>
+                        < Row >
+                            <Col>
+                                <RPItem arg1={item[0]} arg2={item[1]} arg3={item[2]} />
+                            </Col>
+                        </Row>
 
-                    </Row>
-                    <Row>
-                        <Col>
-                        
-                        </Col>
-                    </Row>
-                    </Container>
-    )
+                    );
+                })
+            }
+
+        </Container >
+    );
 }
 
 export default RighPane;
